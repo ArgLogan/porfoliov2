61feb2942ab15054ce40dca533184da898e62eee
JJRoma <jj_roma@yahoo.ccom.ar>
2023-11-26 19:58:37 -0300

    puesta en marcha

---
+++ src/compoents/Banner/index.jsx
@@ -0,0 +1,14 @@
+import './banner.css'
+
+const Banner = (props)=>{
+    return(
+        <div className="c-banner">
+            <img src="./imgs/Banner.jpg" alt="banner"/>
+            <div className="img-perfil">
+                <img src="./imgs/banner2.pmg" alt="" />
+
+            </div>
+        </div>
+    )
+}
+export default Banner
---
+++ src/compoents/navbar/index.jsx
@@ -0,0 +1,12 @@
+import './navbar.css'
+
+const Navbar = (props)=>{
+
+    return(
+        <div className="navbar">
+            <h1>hola</h1>
+            <h1>otro</h1>
+        </div>
+    )
+}
+export default Navbar;
